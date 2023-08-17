9f7c31a752c48bf0280799486fa00f576ac29b70
Israel Oliveira <102741887+IsraelOliver@users.noreply.github.com>
2023-08-17 16:55:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,23 @@ function carregar() {
     var msg = document.querySelector('div.msg')
     var img = document.querySelector('img.img')
     var data = new Date()
-    //var hora = data.getHours()
+    var hora = data.getHours()
 
     //Para testar os horarios direfentes:
-    var hora = 19
+    //var hora = 19
 
     msg.innerHTML = `Agora são ${hora} horas`
 
     if (hora >= 0 && hora < 12) {
-        img.src = 'assents/Manha.png'
+        img.src = 'assets/Manha.png'
         document.body.style.background = "#58c6fe"
 
     } else if (hora >= 12 && hora <= 18) {
-        img.src = 'assents/Tarde.png'
+        img.src = 'assets/Tarde.png'
         document.body.style.background = '#df6f43'
 
     } else {
-        img.src = 'assents/Noite.png'
+        img.src = 'assets/Noite.png'
         document.body.style.background = '#15253e'
 
     }
